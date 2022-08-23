dda0b8bab4097628fe78b2680cfaf1b690b39d7c
希 安保 <mareto76@gmail.com>
2022-08-23 09:28:33 +0900

    a

--- ex03/ex03.cpp
+++ ex03/ex03.cpp
@@ -1,51 +1,48 @@
 ﻿#include <iostream>
 #include<iomanip>
-#include <stdlib.h>
-#include"getBmi.h"
-#include"getObesity.h"
+#include"GetBmi.h"
+#include"GetObesity.h"
+
 using namespace std;
-
-string obesityMessage[] =
-{
-    "低体重",        //[0] -1
-    "普通体重",     //[1] 0
-    "肥満(1度)",   //[2] 1
-    "肥満(2度)",   //[3] 2
-    "肥満(3度)",   //[4] 3
-    "肥満(4度)",   //[5] 4
-};
-
-void testGetObesity() {
-    double testBmi[] = { 18.4,18.5,18.6,24,25,26,29,30,31,34,35,36,39,40,41 };
-    for (int i = 0; i < _countof(testBmi); i++) {
-        cout << "BMI=" << testBmi[i] << ",肥満度=" << obesityMessage[getObesity(testBmi[i])+1] << endl;
-    }
-}
 
 int main()
 {
-    const int STD_BMI = 22; // 標準BMI
-    double height;  // 身長
-    double weight;  // 体重
-    double bmi = 0;    // bmi
-    double stdWeight;   // 標準体重
+    double height;  //身長
+    double weight;  //体重
+    double bmi;  //BMI
+    int obesity;  //肥満度の数値
+    string degreeOfObesity;  //肥満度
 
-    //キー入力をheightに格納する
-    cout << "身長(cm)を入力してください";
+    cout << "身長を入力してください(m) : ";
     cin >> height;
-
-    //キー入力をweightに格納する
-    cout << "体重(kg)を入力してください";
+    cout << "体重を入力してください(kg) : ";
     cin >> weight;
-
-    // 標準体重を計算する
-    stdWeight = STD_BMI * (height / 100) * (height / 100);
-    bmi = getBmi(height, weight);
-    int obesity = getObesity(bmi);
-    // bmiを画面に表示する
+    bmi = GetBmi(height, weight);
     cout << "あなたのBMIは、" << fixed << setprecision(1) << bmi << "です。" << endl;
-    cout << "あなたの肥満度は、" << obesityMessage[ obesity+1] << "です。" << endl;
-    // 標準体重を画面に表示する
-    cout << "あなたの標準体重は、" << stdWeight << "です" << endl;
-    return 0;
+    obesity = GetObesity(bmi);
+    if (obesity == 4)
+    {
+        degreeOfObesity = "肥満(4度)";
+    }
+    else if (obesity == 3)
+    {
+        degreeOfObesity = "肥満(3度)";
+    }
+    else if (obesity == 2)
+    {
+        degreeOfObesity = "肥満(2度)";
+    }
+    else if (obesity == 1)
+    {
+        degreeOfObesity = "肥満(1度)";
+    }
+    else if (obesity == 0)
+    {
+        degreeOfObesity = "普通体重";
+    }
+    else
+    {
+        degreeOfObesity = "低体重";
+    }
+    cout << "あなたの肥満度は" << degreeOfObesity << "です。" << endl;
 }
